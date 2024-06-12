55262e786b82e98c530e816d67d8d17b44c4db6d
BE-KDW <calmnature2@gmail.com>
2024-06-12 18:45:14 +0900

    Step5 4번문제

---
+++ src/Step5/No4.java
@@ -0,0 +1,12 @@
+package Step5;
+
+import java.util.Scanner;
+
+public class No4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int alpha = scan.nextLine().charAt(0);
+        scan.close();
+        System.out.println(alpha);
+    }
+}
