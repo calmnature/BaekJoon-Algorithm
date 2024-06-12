58b74fa7d4d3594298ddb3605d48b6cdfdec6edb
BE-KDW <calmnature2@gmail.com>
2024-06-12 18:28:13 +0900

    Step5 2번문제

---
+++ src/Step5/No2.java
@@ -0,0 +1,13 @@
+package Step5;
+
+import java.util.Scanner;
+
+public class No2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        scan.close();
+        System.out.println(str.length());
+
+    }
+}
