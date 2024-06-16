9de9748d3fd468c5fb4b342ea259f17241a9f2a1
BE-KDW <calmnature2@gmail.com>
2024-06-17 03:12:26 +0900

    Step07 1번문제

---
+++ src/Step07/No1.java
@@ -0,0 +1,37 @@
+package Step07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N][M];
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < N; j++){
+                st = new StringTokenizer(br.readLine());
+                for(int k = 0; k < M; k++){
+                    A[j][k] += Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                sb.append(A[i][j]);
+                if(j + 1 != M)
+                    sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
