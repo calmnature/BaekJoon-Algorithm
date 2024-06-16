913d505347763c91d3797b123678a58ca7e0f4be
BE-KDW <calmnature2@gmail.com>
2024-06-17 04:14:50 +0900

    Step7 3번문제

---
+++ src/Step07/No3.java
@@ -0,0 +1,30 @@
+package Step07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = new String[5];
+        for(int i = 0; i < 5; i++){
+            str[i] = br.readLine();
+        }
+        br.close();
+        // Check
+//        for(String s : str){
+//            System.out.println(s);
+//        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 5; j++){
+                if(i < str[j].length())
+                    sb.append(str[j].charAt(i));
+            }
+        }
+        System.out.println(sb);
+    }
+}
