e1ddaa39b34cf115c3f13762e5b0f72d38774c8a
BE-KDW <calmnature2@gmail.com>
2024-06-17 17:40:27 +0900

    Step8 2번문제

---
+++ src/Step8/No2.java
@@ -0,0 +1,30 @@
+package Step8;
+
+import java.util.Scanner;
+
+public class No2 {
+    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int N = scan.nextInt();
+//        int B = scan.nextInt();
+//        scan.close();
+//        StringBuilder sb = new StringBuilder();
+//        do {
+//            int remainder = N % B;
+//            sb.append(remainder < 10 ? remainder : String.valueOf((char)(remainder - 10 + 'A')));
+//            N/=B;
+//        }while(N != 0);
+//        System.out.println(sb.reverse());
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int B = scan.nextInt();
+        scan.close();
+        StringBuilder sb = new StringBuilder();
+        do {
+            int remainder = N % B;
+            sb.append(remainder < 10 ? remainder : (char)(remainder - 10 + 'A'));
+            N /= B;
+        } while (N != 0);
+        System.out.println(sb.reverse().toString());
+    }
+}
