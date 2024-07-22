b9a410711706381a59a7c374d28a752cd348c0e8
BE-KDW <calmnature@naver.com>
2024-07-22 09:50:43 +0900

    15649번 문제(N과 M 1) - 수정

--- src/Problem_Number/No15649.java
+++ src/Problem_Number/No15649.java
@@ -13,35 +13,37 @@ public class No15649 {
     /*
         - **문제 해결**
             - 1 ~ N개의 수에서 길이가 M인 수열을 공백으로 출력
-            - visited[N] 배열과 arr[M] 배열이 핵심
+            - visited[N] 배열을 이용하여 방문 여부를 확인
     * */
     static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] sequence; // 수열의 배열
-    static boolean[] visited; // 방문 확인 배열
+    static int numCount, length;
+    static boolean[] visited;
+    static int[] arr;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream((br.readLine().split(" "))).mapToInt(Integer::parseInt).toArray();
-        N = input[0]; M = input[1];
-        visited = new boolean[N]; sequence = new int[M];
+        numCount = input[0]; length = input[1];
+        visited = new boolean[numCount]; // 방문 여부를 확인할 배열
+        arr = new int[length]; // 꽉 차면 출력할 배열
         DFS(0);
         System.out.println(sb);
     }
 
-    private static void DFS(int depth) {
-        if(depth == M) { // 재귀의 깊이가 수열의 길이와 같아졌다면
-            Arrays.stream(sequence).forEach(num -> sb.append(num).append(" "));
+    private static void DFS(int depth){
+        // 깊이와 길이가 같아졌다 => 배열이 꽉 찼다
+        if(depth == length){
+            Arrays.stream(arr).forEach(num -> sb.append(num).append(" "));
             sb.append("\n");
             return;
         }
 
-        IntStream.range(0, visited.length)
-                .filter(i -> !visited[i])
+        IntStream.range(0, numCount) // 0 ~ numCount - 1까지 반복
+                .filter(i -> !visited[i]) // 방문하지 않았다면 실행
                 .forEach(i -> {
-                    visited[i] = true;
-                    sequence[depth] = i+1;
-                    DFS(depth + 1);
-                    visited[i] = false;
+                    visited[i] = true; // 방문한 것으로 체크
+                    arr[depth] = i + 1; // 현재 깊이의 방 번호에 값 추가
+                    DFS(depth + 1); // 깊이를 1 늘려 재귀 호출
+                    visited[i] = false; // 재귀가 끝나면 i번 방의 방문을 false로 변경
                 });
     }
 }
